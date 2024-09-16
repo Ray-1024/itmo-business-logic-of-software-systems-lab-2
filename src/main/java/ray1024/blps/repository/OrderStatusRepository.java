8fb8b5e7976cd640885ee1650696df1f009774d7
Dmitriy <74064110+Ray-1024@users.noreply.github.com>
2024-09-16 17:44:21 +0300

    Delete src/main/java/ray1024/blps/repository/OrderStatusRepository.java

--- src/main/java/ray1024/blps/repository/OrderStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package ray1024.blps.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ray1024.blps.model.entity.OrderStatus;
-
-@Repository
-public interface OrderStatusRepository extends JpaRepository<OrderStatus, Long> {
-}
